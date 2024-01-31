78314260e42c8047c6361dcbff6f3d9a2e853d7b
brosenmoller <rosenmollerben@gmail.com>
2024-01-31 14:41:30 +0100

    Optimsed Vertex count + code clean up

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,10 @@
 mod schematic;
-mod mesh_generation;
 mod obj_generation;
 
-use std::{fs::File, io::{self, Write}};
+use std::{fs::File, io::Write};
 
-use obj::Obj;
-use schematic::{load_schematic, Schematic};
-use mesh_generation::generate_mesh;
-use obj_generation::generate_obj;
+use schematic::load_schematic;
+use obj_generation::generate_obj_string;
 
 const PATH: &str = "resources/DarkOakTree1.schem";
 const FILE_NAME: &str = "tree";
@@ -21,7 +18,7 @@ fn main() {
             // println!("{}\n", schematic.block_data[5]);
             // println!("{:?}", schematic);
 
-            let obj = generate_obj_file(&schematic);
+            let obj = generate_obj_string(&schematic);
             let mut file = File::create(FILE_NAME.to_owned() +".obj").unwrap();
             write!(&mut file, "{}\n", obj).unwrap();
 
@@ -34,124 +31,4 @@ fn main() {
 
     // EVERYTHING
     //let schematic: Result<Value> = from_bytes(data.as_slice());
-}
-
-
-fn test_export_to_obj_cube(filename: &str) -> io::Result<()> {
-    let mut file = File::create(filename)?;
-
-    write!(
-        &mut file,
-        "# Cube.obj\n
-
-        # Vertices\n
-        v -1.0 -1.0 -1.0\n
-        v -1.0 -1.0  1.0\n
-        v -1.0  1.0 -1.0\n
-        v -1.0  1.0  1.0\n
-        v  1.0 -1.0 -1.0\n
-        v  1.0 -1.0  1.0\n
-        v  1.0  1.0 -1.0\n
-        v  1.0  1.0  1.0\n
-        
-        # Faces\n
-        f 1 2 4 3\n
-        f 5 6 8 7\n
-        f 1 2 6 5\n
-        f 3 4 8 7\n
-        f 1 3 7 5\n
-        f 2 4 8 6\n",
-    )?;
-
-    Ok(())
-}
-
-fn generate_obj_file(schematic: &Schematic) -> String {
-
-    let mut obj_string: String = String::new();
-    let mut index_count: usize = 0;
-
-    for y in 0..schematic.height {
-        for z in 0..schematic.width {
-            for x in 0..schematic.length {
-                let index = ((y * schematic.length + z) * schematic.width + x) as usize;
-
-                if index >= schematic.block_data.len() { continue; }
-
-                let cube_value = schematic.block_data[index];
-
-                let width = schematic.width as usize;
-                let height = schematic.height as usize;
-                let length = schematic.length as usize;
-
-                if cube_value == 0 { continue; }
-
-                // Check if adjacent cubes obstruct the face
-                let left_obstructed = x > 0 && schematic.block_data[index - 1] > 0;
-                let right_obstructed = x < schematic.length - 1 && schematic.block_data[index + 1] > 0;
-                let front_obstructed = z > 0 && schematic.block_data[index - width] > 0;
-                let back_obstructed = z < schematic.width - 1 && schematic.block_data[index + width] > 0;
-                let bottom_obstructed = y > 0 && schematic.block_data[index - length * width] > 0;
-                let top_obstructed = y < schematic.length - 1 && schematic.block_data[index + length * width] > 0;
-
-                obj_string.push_str(&format!("o cube_{}_{}_{}\n", x, y, z));
-
-                // Only write vertices and faces if the corresponding face is not obstructed
-                obj_string.push_str(&format!("v {} {} {}\n", x, y, z));
-                obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z));
-                obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z + 1));
-                obj_string.push_str(&format!("v {} {} {}\n", x, y, z + 1));
-
-                obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z));
-                obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z));
-                obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z + 1));
-                obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z + 1));
-
-                obj_string.push_str(&format!("v {} {} {}\n", x, y, z));
-                obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z));
-                obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z));
-                obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z));
-
-                obj_string.push_str(&format!("v {} {} {}\n", x, y, z + 1));
-                obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z + 1));
-                obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z + 1));
-                obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z + 1));
-
-                obj_string.push_str(&format!("v {} {} {}\n", x, y, z));
-                obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z));
-                obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z + 1));
-                obj_string.push_str(&format!("v {} {} {}\n", x, y, z + 1));
-
-                obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z));
-                obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z));
-                obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z + 1));
-                obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z + 1));
-
-                // Write the corresponding faces
-                if !left_obstructed {
-                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count + 1, index_count + 2, index_count + 3, index_count + 4));
-                }
-                if !right_obstructed {
-                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count + 5, index_count + 6, index_count + 7, index_count + 8));
-                }
-                if !front_obstructed {
-                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count + 9, index_count + 10, index_count + 11, index_count + 12));
-                }
-                if !back_obstructed {
-                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count + 13, index_count + 14, index_count + 15, index_count + 16));
-                }
-                if !bottom_obstructed {
-                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count + 17, index_count + 18, index_count + 19, index_count + 20));
-                }
-                if !top_obstructed {
-                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count + 21, index_count + 22, index_count + 23, index_count + 24));
-                }
-
-                index_count += 24;
-                
-            }
-        }
-    }
-
-    obj_string
 }
--- src/obj_generation.rs
+++ src/obj_generation.rs
@@ -1,225 +1,91 @@
 use crate::schematic;
 use schematic::Schematic;
 
+pub fn generate_obj_string(schematic: &Schematic) -> String {
 
-pub fn generate_obj(schematic: &Schematic) -> String {
     let mut obj_string: String = String::new();
+    let mut index_count: usize = 1;
 
-    let mut index_count: usize = 0;
+    for y in 0..schematic.height {
+        for z in 0..schematic.width {
+            for x in 0..schematic.length {
+                let index = ((y * schematic.length + z) * schematic.width + x) as usize;
 
-    for x in 0..schematic.width {
-        for y in 0..schematic.height {
-            for z in 0..schematic.length {
-                let index: usize = ((y * schematic.length + z) * schematic.width + x) as usize;
-                let value: i8 = schematic.block_data[index];
+                if index >= schematic.block_data.len() { continue; }
 
-                if value == 0 { continue; }
-
-                let xf = x as f32;
-                let yf = y as f32;
-                let zf = z as f32;
+                let cube_value = schematic.block_data[index];
 
                 let width = schematic.width as usize;
-                let height = schematic.height as usize;
                 let length = schematic.length as usize;
-                
+
+                if cube_value == 0 { continue; }
+
+                // Check if adjacent cubes obstruct the face
+                let left_obstructed = x > 0 && schematic.block_data[index - 1] > 0;
+                let right_obstructed = x < schematic.length - 1 && schematic.block_data[index + 1] > 0;
+                let front_obstructed = z > 0 && schematic.block_data[index - width] > 0;
+                let back_obstructed = z < schematic.width - 1 && schematic.block_data[index + width] > 0;
+                let bottom_obstructed = y > 0 && schematic.block_data[index - length * width] > 0;
+                let top_obstructed = y < schematic.length - 1 && schematic.block_data[index + length * width] > 0;
+
                 obj_string.push_str(&format!("o cube_{}_{}_{}\n", x, y, z));
 
-                obj_string.push_str(&format!("v {} {} {}\n", xf, yf, zf));
-                obj_string.push_str(&format!("v {} {} {}\n", xf + 1.0, yf, zf));
-                obj_string.push_str(&format!("v {} {} {}\n", xf + 1.0, yf, zf + 1.0));
-                obj_string.push_str(&format!("v {} {} {}\n", xf, yf, zf + 1.0));
-                obj_string.push_str(&format!("v {} {} {}\n", xf, yf + 1.0, zf));
-                obj_string.push_str(&format!("v {} {} {}\n", xf + 1.0, yf + 1.0, zf));
-                obj_string.push_str(&format!("v {} {} {}\n", xf + 1.0, yf + 1.0, zf + 1.0));
-                obj_string.push_str(&format!("v {} {} {}\n", xf, yf + 1.0, zf + 1.0));
+                if !left_obstructed {
+                    obj_string.push_str(&format!("v {} {} {}\n", x, y, z));
+                    obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z));
+                    obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z + 1));
+                    obj_string.push_str(&format!("v {} {} {}\n", x, y, z + 1));
+                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count, index_count + 1, index_count + 2, index_count + 3));
+                    index_count += 4;
+                }
 
-                // add_face(&mut obj_string, index_count, 1, 4, 3, 2, 
-                //     x > 0 && schematic.block_data[index - 1] == 0
-                // );
-                // add_face(&mut obj_string, index_count, 2, 6, 7, 3, 
-                //     x < schematic.width - 1 && schematic.block_data[index + 1] == 0
-                // );
-                // add_face(&mut obj_string, index_count, 4, 3, 7, 8, 
-                //     z < schematic.length - 1 && schematic.block_data[index + width] == 0
-                // );
-                // add_face(&mut obj_string, index_count, 5, 1, 4, 8, 
-                //     z > 0 && schematic.block_data[index - width] == 0
-                // );
-                // add_face(&mut obj_string, index_count, 5, 6, 2, 1, 
-                //     y > 0 && schematic.block_data[index - width * length] == 0
-                // );
-                // add_face(&mut obj_string, index_count, 8, 7, 6, 5, 
-                //     y < schematic.height - 1 && schematic.block_data[index + width * length] == 0
-                // );
+                if !right_obstructed {
+                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z));
+                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z));
+                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z + 1));
+                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z + 1));
+                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count, index_count + 1, index_count + 2, index_count + 3));
+                    index_count += 4;
+                }
 
-                add_face(&mut obj_string, index_count, 1, 4, 3, 2, 
-                    x == 0 || schematic.block_data[index - 1] == 0
-                );
-                add_face(&mut obj_string, index_count, 2, 6, 7, 3, 
-                    x == schematic.width - 1 || schematic.block_data[index + 1] == 0
-                );
-                add_face(&mut obj_string, index_count, 4, 3, 7, 8, 
-                    z == schematic.length - 1 || schematic.block_data[index + width] == 0
-                );
-                add_face(&mut obj_string, index_count, 5, 1, 4, 8, 
-                    z == 0 || schematic.block_data[index - width] == 0
-                );
-                add_face(&mut obj_string, index_count, 5, 6, 2, 1, 
-                    y == 0 || schematic.block_data[index - width * length] == 0
-                );
-                add_face(&mut obj_string, index_count, 8, 7, 6, 5, 
-                    y == schematic.height - 1 || schematic.block_data[index + width * length] == 0
-                );
-                
-                // add_face(&mut obj_string, index_count, 1, 4, 3, 2, 
-                //     x == 0 || schematic.block_data[index - 1] == 0
-                // );
-                // add_face(&mut obj_string, index_count, 2, 6, 7, 3, 
-                //     x == schematic.width - 1 || schematic.block_data[index + 1] == 0
-                // );
-                // add_face(&mut obj_string, index_count, 4, 3, 7, 8, 
-                //     z == schematic.length - 1 || schematic.block_data[index + width] == 0
-                // );
-                // add_face(&mut obj_string, index_count, 5, 1, 4, 8, 
-                //     z == 0 || schematic.block_data[index - width] == 0
-                // );
-                // add_face(&mut obj_string, index_count, 5, 6, 2, 1, 
-                //     y == 0 || schematic.block_data[((y as usize + 1) * length + z as usize) * width + x as usize] == 0
-                // );
-                // add_face(&mut obj_string, index_count, 8, 7, 6, 5, 
-                //     y == schematic.height - 1 || schematic.block_data[((y as usize - 1) * length + z as usize) * width + x as usize] == 0
-                // );
+                if !front_obstructed {
+                    obj_string.push_str(&format!("v {} {} {}\n", x, y, z));
+                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z));
+                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z));
+                    obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z));
+                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count, index_count + 1, index_count + 2, index_count + 3));
+                    index_count += 4;
+                }
 
-                index_count += 8;
-                
-                // Generate face if block is on the outside or has no block next to it
-                // {
-                //     let side_index: usize = ((y * schematic.length + z) * schematic.width + (x + 1)) as usize;
+                if !back_obstructed {
+                    obj_string.push_str(&format!("v {} {} {}\n", x, y, z + 1));
+                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z + 1));
+                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z + 1));
+                    obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z + 1));
+                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count, index_count + 1, index_count + 2, index_count + 3));
+                    index_count += 4;
+                }
 
-                //     if side_index > schematic.block_data.len() || x == schematic.width - 1 || schematic.block_data[side_index] != 0 {
-                
-                //         obj_string.push_str(&format!("f {} {} {} {}\n", 
-                //             2 + index_count, 6 + index_count, 7 + index_count, 3 + index_count)
-                //         );
+                if !bottom_obstructed {
+                    obj_string.push_str(&format!("v {} {} {}\n", x, y, z));
+                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z));
+                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z + 1));
+                    obj_string.push_str(&format!("v {} {} {}\n", x, y, z + 1));
+                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count, index_count + 1, index_count + 2, index_count + 3));
+                    index_count += 4;
+                }
 
-                //     }
-                // }
-
-                // {
-                //     let side_index: usize = ((y * schematic.length + z) * schematic.width + (x - 1)) as usize;
-
-                //     if  side_index > schematic.block_data.len() || x == 0 || schematic.block_data[side_index] != 0 {
-                
-                //         obj_string.push_str(&format!("f {} {} {} {}\n", 
-                //             1 + index_count, 4 + index_count, 8 + index_count, 5 + index_count)
-                //         );
-                        
-                //     }
-                // }
-
-                // {
-                //     let side_index: usize = (((y + 1) * schematic.length + z) * schematic.width + x) as usize;
-
-                //     if  side_index > schematic.block_data.len() || y == schematic.height - 1 || schematic.block_data[side_index] != 0 {
-                
-                //         obj_string.push_str(&format!("f {} {} {} {}\n", 
-                //             5 + index_count, 8 + index_count, 7 + index_count, 6 + index_count)
-                //         );
-                        
-                //     }
-                // }
-
-                // {
-                //     let side_index: usize = (((y - 1) * schematic.length + z) * schematic.width + x) as usize;
-
-                //     if side_index > schematic.block_data.len() || y == 0 || schematic.block_data[side_index] != 0 {
-                    
-                //         obj_string.push_str(&format!("f {} {} {} {}\n", 
-                //             1 + index_count, 2 + index_count, 3 + index_count, 4 + index_count)
-                //         );
-                        
-                //     }
-                // }
-                
-                // {
-                //     let side_index: usize = ((y * schematic.length + (z + 1)) * schematic.width + x) as usize;
-
-                //     if  side_index > schematic.block_data.len() || z == schematic.length - 1 || schematic.block_data[side_index] != 0 {
-                
-                //         obj_string.push_str(&format!("f {} {} {} {}\n", 
-                //             4 + index_count, 3 + index_count, 7 + index_count, 8 + index_count)
-                //         );
-                        
-                //     }
-                // }
-
-                // {
-                //     let side_index: usize = ((y * schematic.length + (z - 1)) * schematic.width + x) as usize;
-
-                //     if side_index > schematic.block_data.len() || z == 0 || schematic.block_data[side_index] != 0 {
-                
-                //         obj_string.push_str(&format!("f {} {} {} {}\n", 
-                //             1 + index_count, 5 + index_count, 6 + index_count, 2 + index_count)
-                //         );
-                        
-                //     }
-                // }
-                
-                // index_count += 8;
-                
+                if !top_obstructed {
+                    obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z));
+                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z));
+                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z + 1));
+                    obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z + 1));
+                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count, index_count + 1, index_count + 2, index_count + 3));
+                    index_count += 4;
+                }
             }
         }
     }
 
     obj_string
 }
-
-fn add_face(obj_string: &mut String, index_count: usize, i1: usize, i2: usize, i3: usize, i4: usize, not_obstructed: bool) {
-    if not_obstructed {
-        obj_string.push_str(&format!("f {} {} {} {}\n", i1 + index_count, i2 + index_count, i3 + index_count, i4 + index_count));
-    }
-}
-
-// pub fn generate_obj(schematic: &Schematic) -> String {
-//     let mut obj_string: String = String::new();
-
-//     let mut index_count: usize = 0;
-
-//     for x in 0..schematic.width {
-//         for y in 0..schematic.height {
-//             for z in 0..schematic.length {
-//                 let index: usize = ((y * schematic.length + z) * schematic.width + x) as usize;
-//                 let value: i8 = schematic.block_data[index];
-
-//                 if value != 0 {
-//                     let xf = x as f32;
-//                     let yf = y as f32;
-//                     let zf = z as f32;
-                    
-//                     obj_string.push_str(&format!("o cube_{}_{}_{}\n", x, y, z));
-
-//                     obj_string.push_str(&format!("v {} {} {}\n", xf, yf, zf));
-//                     obj_string.push_str(&format!("v {} {} {}\n", xf + 1.0, yf, zf));
-//                     obj_string.push_str(&format!("v {} {} {}\n", xf + 1.0, yf, zf + 1.0));
-//                     obj_string.push_str(&format!("v {} {} {}\n", xf, yf, zf + 1.0));
-//                     obj_string.push_str(&format!("v {} {} {}\n", xf, yf + 1.0, zf));
-//                     obj_string.push_str(&format!("v {} {} {}\n", xf + 1.0, yf + 1.0, zf));
-//                     obj_string.push_str(&format!("v {} {} {}\n", xf + 1.0, yf + 1.0, zf + 1.0));
-//                     obj_string.push_str(&format!("v {} {} {}\n", xf, yf + 1.0, zf + 1.0));
-
-//                     obj_string.push_str(&format!("f {} {} {} {}\n", 1 + index_count, 2 + index_count, 3 + index_count, 4 + index_count));
-//                     obj_string.push_str(&format!("f {} {} {} {}\n", 5 + index_count, 8 + index_count, 7 + index_count, 6 + index_count));
-//                     obj_string.push_str(&format!("f {} {} {} {}\n", 2 + index_count, 6 + index_count, 7 + index_count, 3 + index_count));
-//                     obj_string.push_str(&format!("f {} {} {} {}\n", 1 + index_count, 4 + index_count, 8 + index_count, 5 + index_count));
-//                     obj_string.push_str(&format!("f {} {} {} {}\n", 1 + index_count, 5 + index_count, 6 + index_count, 2 + index_count));
-//                     obj_string.push_str(&format!("f {} {} {} {}\n", 4 + index_count, 3 + index_count, 7 + index_count, 8 + index_count));
-                    
-//                     index_count += 8;
-//                 }
-//             }
-//         }
-//     }
-
-//     obj_string
-// }
