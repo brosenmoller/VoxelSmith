f6ec1a7ca7aa8369dd75b32db44782a37f1d9849
brosenmoller <rosenmollerben@gmail.com>
2024-02-02 00:45:57 +0100

    Vertex Normal started

--- src/main.rs
+++ src/main.rs
@@ -6,29 +6,22 @@ use std::{fs::File, io::Write};
 use schematic::load_schematic;
 use obj_generation::generate_obj_string;
 
-const PATH: &str = "resources/DarkOakTree1.schem";
-const FILE_NAME: &str = "tree";
+const PATH: &str = "resources/test_schematics/DarkOakTree1.schem";
+const FILE_NAME: &str = "resources/test_obj/tree2";
 
 fn main() {
     let schematic = load_schematic(PATH);
 
     match schematic {
         Ok(schematic) => {
-            // println!("{}\n", schematic.block_data[0]);
-            // println!("{}\n", schematic.block_data[5]);
-            // println!("{:?}", schematic);
-
             let obj = generate_obj_string(&schematic);
             let mut file = File::create(FILE_NAME.to_owned() +".obj").unwrap();
             write!(&mut file, "{}\n", obj).unwrap();
-
-            //let _ = export_to_obj_file(&obj, "out.obj");
         }
         Err(err) => {
             eprintln!("Error decoding schematic: {:?}", err);
         }
     }
 
-    // EVERYTHING
     //let schematic: Result<Value> = from_bytes(data.as_slice());
 }
--- src/obj_generation.rs
+++ src/obj_generation.rs
@@ -4,7 +4,15 @@ use schematic::Schematic;
 pub fn generate_obj_string(schematic: &Schematic) -> String {
 
     let mut obj_string: String = String::new();
-    let mut index_count: usize = 1;
+    let mut v_count: usize = 1;
+
+    // normals
+    obj_string.push_str(&format!("vn {} {} {}\n", -1, 0, 0));    // 1
+    obj_string.push_str(&format!("vn {} {} {}\n", 1, 0, 0));     // 2
+    obj_string.push_str(&format!("vn {} {} {}\n", 0, 0, -1));    // 3
+    obj_string.push_str(&format!("vn {} {} {}\n", 0, 0, 1));     // 4
+    obj_string.push_str(&format!("vn {} {} {}\n", 0, -1, 0));    // 5
+    obj_string.push_str(&format!("vn {} {} {}\n", 0, 1, 0));     // 6
 
     for y in 0..schematic.height {
         for z in 0..schematic.width {
@@ -35,8 +43,12 @@ pub fn generate_obj_string(schematic: &Schematic) -> String {
                     obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z));
                     obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z + 1));
                     obj_string.push_str(&format!("v {} {} {}\n", x, y, z + 1));
-                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count, index_count + 1, index_count + 2, index_count + 3));
-                    index_count += 4;
+
+                    obj_string.push_str(&format!(
+                        "f {}//{} {}//{} {}//{} {}//{}\n", 
+                        v_count, 1, v_count + 1, 1, v_count + 2, 1, v_count + 3, 1)
+                    );
+                    v_count += 4;
                 }
 
                 if !right_obstructed {
@@ -44,17 +56,12 @@ pub fn generate_obj_string(schematic: &Schematic) -> String {
                     obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z));
                     obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z + 1));
                     obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z + 1));
-                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count, index_count + 1, index_count + 2, index_count + 3));
-                    index_count += 4;
-                }
 
-                if !front_obstructed {
-                    obj_string.push_str(&format!("v {} {} {}\n", x, y, z));
-                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z));
-                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z));
-                    obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z));
-                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count, index_count + 1, index_count + 2, index_count + 3));
-                    index_count += 4;
+                    obj_string.push_str(&format!(
+                        "f {}//{} {}//{} {}//{} {}//{}\n", 
+                        v_count, 2, v_count + 1, 2, v_count + 2, 2, v_count + 3, 2)
+                    );
+                    v_count += 4;
                 }
 
                 if !back_obstructed {
@@ -62,8 +69,25 @@ pub fn generate_obj_string(schematic: &Schematic) -> String {
                     obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z + 1));
                     obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z + 1));
                     obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z + 1));
-                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count, index_count + 1, index_count + 2, index_count + 3));
-                    index_count += 4;
+
+                    obj_string.push_str(&format!(
+                        "f {}//{} {}//{} {}//{} {}//{}\n", 
+                        v_count, 3, v_count + 1, 3, v_count + 2, 3, v_count + 3, 3)
+                    );
+                    v_count += 4;
+                }
+
+                if !front_obstructed {
+                    obj_string.push_str(&format!("v {} {} {}\n", x, y, z));
+                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z));
+                    obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z));
+                    obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z));
+
+                    obj_string.push_str(&format!(
+                        "f {}//{} {}//{} {}//{} {}//{}\n", 
+                        v_count, 4, v_count + 1, 4, v_count + 2, 4, v_count + 3, 4)
+                    );
+                    v_count += 4;
                 }
 
                 if !bottom_obstructed {
@@ -71,8 +95,12 @@ pub fn generate_obj_string(schematic: &Schematic) -> String {
                     obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z));
                     obj_string.push_str(&format!("v {} {} {}\n", x + 1, y, z + 1));
                     obj_string.push_str(&format!("v {} {} {}\n", x, y, z + 1));
-                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count, index_count + 1, index_count + 2, index_count + 3));
-                    index_count += 4;
+
+                    obj_string.push_str(&format!(
+                        "f {}//{} {}//{} {}//{} {}//{}\n", 
+                        v_count, 5, v_count + 1, 5, v_count + 2, 5, v_count + 3, 5)
+                    );
+                    v_count += 4;
                 }
 
                 if !top_obstructed {
@@ -80,8 +108,12 @@ pub fn generate_obj_string(schematic: &Schematic) -> String {
                     obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z));
                     obj_string.push_str(&format!("v {} {} {}\n", x + 1, y + 1, z + 1));
                     obj_string.push_str(&format!("v {} {} {}\n", x, y + 1, z + 1));
-                    obj_string.push_str(&format!("f {} {} {} {}\n", index_count, index_count + 1, index_count + 2, index_count + 3));
-                    index_count += 4;
+
+                    obj_string.push_str(&format!(
+                        "f {}//{} {}//{} {}//{} {}//{}\n", 
+                        v_count, 6, v_count + 1, 6, v_count + 2, 6, v_count + 3, 6)
+                    );
+                    v_count += 4;
                 }
             }
         }
