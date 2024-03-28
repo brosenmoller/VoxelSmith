634579849c4e740335ea5e48aea8385e94ae48df
brosenmoller <rosenmollerben@gmail.com>
2024-03-28 17:07:20 +0100

    Quick Import

---
+++ Assets/Scripts/UI/Buttons/QuickImport.cs
@@ -0,0 +1,23 @@
+﻿using Godot;
+
+public partial class QuickImport : MarginContainer
+{
+    private Button button;
+
+    public override void _Ready()
+    {
+        button = this.GetChildByType<Button>();
+
+        button.Pressed += OnConfirmed;
+    }
+
+    private void OnConfirmed()
+    {
+        if (GameManager.DataManager.EditorData.importPaths.ContainsKey(GameManager.DataManager.ProjectData.id))
+        {
+            EditorData.ImportSettings importSettings = GameManager.DataManager.EditorData.importPaths[GameManager.DataManager.ProjectData.id];
+            GameManager.ImportManager.ImportMinecraftSchematic(importSettings.path);
+        }
+    }
+}
+
