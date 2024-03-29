7d7460331a346cf5da5668776f8861263010bddf
brosenmoller <rosenmollerben@gmail.com>
2024-03-29 20:16:55 +0100

    Added Sensible Default PaletteData

--- Assets/Scripts/Management/DataManager/Data/PaletteData.cs
+++ Assets/Scripts/Management/DataManager/Data/PaletteData.cs
@@ -27,21 +27,75 @@ public class PaletteData
     {
         PaletteData data = new();
 
-        data.paletteColors.Add(Guid.NewGuid(), new VoxelColor() { color = Color.Color8(0, 0, 0) });
-        data.paletteColors.Add(Guid.NewGuid(), new VoxelColor() { color = Color.Color8(255, 0, 0) });
-        data.paletteColors.Add(Guid.NewGuid(), new VoxelColor() { color = Color.Color8(0, 255, 0) });
-        data.paletteColors.Add(Guid.NewGuid(), new VoxelColor() { color = Color.Color8(0, 0, 255) });
-        data.paletteColors.Add(Guid.NewGuid(), new VoxelColor() { color = Color.Color8(255, 255, 0) });
-        data.paletteColors.Add(Guid.NewGuid(), new VoxelColor() { color = Color.Color8(255, 0, 255) });
-        data.paletteColors.Add(Guid.NewGuid(), new VoxelColor() { color = Color.Color8(0, 255, 255) });
-        data.paletteColors.Add(Guid.NewGuid(), new VoxelColor() { color = Color.Color8(255, 255, 255) });
-
-        data.palletePrefabs.Add(Guid.NewGuid(), new VoxelPrefab() { 
-            color = Color.Color8(100, 200, 0), 
-            unityPrefabGuid = "67ce479430c155e4cbcd3bb0ef4f4954", 
-            prefabName = "TestSpehere", 
-            unityPrefabTransformFileId = "726921523353226827"
+        data.paletteColors.Add(Guid.NewGuid(), new VoxelColor() 
+        { 
+            color = Color.Color8(95, 135, 87), 
+            minecraftIDlist = new List<string>() 
+            {
+                "minecraft:grass_block"
+            }
         });
+
+        data.paletteColors.Add(Guid.NewGuid(), new VoxelColor()
+        {
+            color = Color.Color8(165, 125, 92),
+            minecraftIDlist = new List<string>()
+            {
+                "minecraft:dirt"
+            }
+        });
+
+        data.paletteColors.Add(Guid.NewGuid(), new VoxelColor()
+        {
+            color = Color.Color8(120, 120, 120),
+            minecraftIDlist = new List<string>()
+            {
+                "minecraft:stone"
+            }
+        });
+
+        data.paletteColors.Add(Guid.NewGuid(), new VoxelColor()
+        {
+            color = Color.Color8(110, 110, 110),
+            minecraftIDlist = new List<string>()
+            {
+                "minecraft:cobblestone"
+            }
+        });
+
+        data.paletteColors.Add(Guid.NewGuid(), new VoxelColor()
+        {
+            color = Color.Color8(129, 42, 37),
+            minecraftIDlist = new List<string>()
+            {
+                "minecraft:red_concrete"
+            }
+        });
+
+        data.paletteColors.Add(Guid.NewGuid(), new VoxelColor()
+        {
+            color = Color.Color8(43, 45, 137),
+            minecraftIDlist = new List<string>()
+            {
+                "minecraft:blue_concrete"
+            }
+        });
+
+        data.paletteColors.Add(Guid.NewGuid(), new VoxelColor()
+        {
+            color = Color.Color8(239, 237, 231),
+            minecraftIDlist = new List<string>()
+            {
+                "minecraft:smooth_quartz"
+            }
+        });
+
+        //data.palletePrefabs.Add(Guid.NewGuid(), new VoxelPrefab() { 
+        //    color = Color.Color8(100, 200, 0), 
+        //    unityPrefabGuid = "67ce479430c155e4cbcd3bb0ef4f4954", 
+        //    prefabName = "TestSpehere", 
+        //    unityPrefabTransformFileId = "726921523353226827"
+        //});
 
         return data;
     }
