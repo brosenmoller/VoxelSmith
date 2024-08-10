9a9b3a4f0eec25b92d59daf5c69d8a60f2d8867d
Mia vreven <39905553+naxmaardur@users.noreply.github.com>
2024-08-10 14:42:15 +0200

    Fix Export UVs

--- Assets/Scripts/Generation/CubeValues.cs
+++ Assets/Scripts/Generation/CubeValues.cs
@@ -11,13 +11,13 @@ public static class CubeValues
 
     public static readonly Vector3[] cubeVertices =
     {
-        new(0, 0, 0), 
+        new(0, 0, 0),
         new(1, 0, 0),
-        new(1, 0, 1), 
+        new(1, 0, 1),
         new(0, 0, 1),
-        new(0, 1, 0), 
+        new(0, 1, 0),
         new(1, 1, 0),
-        new(1, 1, 1), 
+        new(1, 1, 1),
         new(0, 1, 1),
     };
 
@@ -34,9 +34,9 @@ public static class CubeValues
     public static readonly Vector2[] cubeUVs =
     {
         new(0, 0),
+        new(1, 1),
         new(1, 0),
         new(0, 1),
-        new(1, 1),
     };
 
     public static readonly int[][] cubeVertexFaceIndices = new int[][]
@@ -51,11 +51,11 @@ public static class CubeValues
 
     public static readonly int[][] cubeUVFaceIndices = new int[][]
     {
-        new[] { 0, 3, 1, 0, 2, 3 },
-        new[] { 1, 2, 0, 1, 3, 2 },
-        new[] { 0, 1, 3, 0, 2, 3 },
-        new[] { 2, 3, 0, 3, 1, 0 },
-        new[] { 0, 1, 3, 3, 2, 0 },
-        new[] { 1, 2, 0, 1, 3, 2 },
+        new[] { 0, 3, 2, 0, 1, 3 },
+        new[] { 0, 3, 2, 0, 1, 3 },
+        new[] { 0, 3, 2, 0, 1, 3 },
+        new[] { 0, 1, 2, 1, 3, 2 },
+        new[] { 0, 3, 1, 1, 2, 0 },
+        new[] { 0, 3, 2, 0, 1, 3 },
     };
 }
