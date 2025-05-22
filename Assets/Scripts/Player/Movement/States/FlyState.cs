1dbb7b292245497ff79b0231e0873460ef3fe467
brosenmoller <rosenmollerben@gmail.com>
2025-05-22 17:31:18 +0200

    Turn off speedup and slow down when flying

--- Assets/Scripts/Player/Movement/States/FlyState.cs
+++ Assets/Scripts/Player/Movement/States/FlyState.cs
@@ -23,16 +23,8 @@ public class FlyState : State<PlayerMovement>
         Vector2 inputDir = Input.GetVector("left", "right", "up", "down");
         Vector3 direction = (ctx.pivot.Transform.Basis * new Vector3(inputDir.X, 0, inputDir.Y)).Normalized();
 
-        if (direction != Vector3.Zero)
-        {
-            tempVelocity.X = direction.X * ctx.horizontalFlySpeed;
-            tempVelocity.Z = direction.Z * ctx.horizontalFlySpeed;
-        }
-        else
-        {
-            tempVelocity.X = (float)Mathf.Lerp(tempVelocity.X, direction.X * ctx.horizontalFlySpeed, delta * 7.0f);
-            tempVelocity.Z = (float)Mathf.Lerp(tempVelocity.Z, direction.Z * ctx.horizontalFlySpeed, delta * 7.0f);
-        }
+        tempVelocity.X = direction.X * ctx.horizontalFlySpeed;
+        tempVelocity.Z = direction.Z * ctx.horizontalFlySpeed;
 
         ctx.Velocity = tempVelocity;
 
